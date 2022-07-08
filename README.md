4907490d0ebcf7ac9adf369cd94290e10ada02b8
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2022-07-08 05:41:08 +0000

    theme updates

---
+++ README.md
@@ -0,0 +1,156 @@
+<p align="center">
+  <img align="center" width="100" src="https://acegif.com/wp-content/gif/thinking-emoji-30.gif" />
+
+  <h1 align="center">Github Readme Riddle</h1>
+  <h3 align="center">Developer Riddle for your Github Readme.</h3>
+</p>
+
+<!-- Badges -->
+<p align="center">
+  <a href="https://github.com/CodeWhiteWeb/github-readme-riddle/issues">
+    <img src="https://img.shields.io/github/issues/CodeWhiteWeb/github-readme-riddle?style=flat-square">
+  </a>
+
+  <a href="https://github.com/CodeWhiteWeb/github-readme-riddle/pulls">
+    <img src="https://img.shields.io/github/issues-pr/CodeWhiteWeb/github-readme-riddle?style=flat-square">
+  </a>
+  
+  <a href="https://twitter.com/CodeWhiteWeb1" target="_blank">
+    <img alt="Twitter: CodeWhiteWeb1" src="https://img.shields.io/twitter/follow/CodeWhiteWeb1.svg?style=social" />
+  </a>
+</p>
+
+<!-- Links -->
+<p align="center">
+  <a href="https://github-readme-riddle.vercel.app/api" target="_blank">View Demo</a>
+  <span> · </span>
+  <a href="https://github.com/CodeWhiteWeb/github-readme-riddle/issues" target="_blank">Report Bug</a>
+</p>
+
+## Features ✨
+
+- [Dynamic riddle in your GitHub Readme.](#Demo)
+- [Create your own riddle.](#Custom)
+
+## Usage ⚡️
+
+Copy the markdown below and paste it in your Github Readme.
+
+```md
+[![Readme Riddle](https://github-readme-riddle.vercel.app/api?type=horizontal&theme=dark)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+```
+
+### Options
+
+| Options | Default  | Available                                                                             |
+| ------- | -------- | ------------------------------------------------------------------------------------- |
+| type    | vertical | horizontal, vertical                                                                  |
+| theme   | light    | dark, chartreuse-dark, radical, merko, gruvbox, tokyonight, algolia, monokai, dracula |
+| myriddle | -        | Customize your riddle                                                                  |
+
+## Installation and Development 🚀
+
+To get started with developing this app locally, here are the steps:
+
+Install the dependencies for the app using,
+
+```sh
+npm install
+# or
+yarn
+```
+
+And, then run the app using,
+
+```sh
+npx vercel dev
+# or
+yarn run vercel dev
+```
+
+## Demo
+
+### Vertical
+
+> You don't need to add **?type=vertical** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?type=vertical)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Horizontal
+
+> You need to add **?type=horizontal** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?type=horizontal)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Light
+
+> You don't need to add **?theme=light** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?type=vertical)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Dark
+
+> You need to add **?theme=dark** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?theme=dark)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Catppuccin
+
+> You need to add **?theme=catppuccin** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?theme=catppuccin)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Algolia
+
+> You need to add **?theme=algolia** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?theme=algolia)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Monokai
+
+> You need to add **?theme=monokai** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?theme=monokai)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+### Dracula
+
+> You need to add **?theme=dracula** parameter.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?theme=dracula)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+## Custom
+
+> You need to add **?myriddle=Your riddle** parameter.
+> URL encode your riddle.
+
+[![readme Riddle](https://github-readme-riddle.vercel.app/api?myriddle=My%20riddle)](https://github.com/CodeWhiteWeb/github-readme-riddle)
+
+## Adding Soon 🚧
+
+- Customization Options
+- More features
+
+## Contributing 🤝
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/CodeWhiteWeb/github-readme-riddle/issues).
+
+## Special Thanks ❤
+
+- Inspired from [AnuragHazra/github-readme-stats](https://github.com/anuraghazra/github-readme-stats) & [PiyushSuthar/github-readme-quotes](https://github.com/PiyushSuthar/github-readme-quotes).
+- [nkilm/riddles-api](https://github.com/nkilm/riddles-api) for Riddle API.
+
+## Show your support 🌈
+
+Be sure to drop a 🌟 if you like the project!
+
+## Author 🤗
+
+👤 **CodeWhiteWeb**
+
+- Website: https://CodeWhiteWeb.cf
+- Twitter: [@CodeWhiteWeb1](https://twitter.com/CodeWhiteWeb1)
+- Github: [@CodeWhiteWeb](https://github.com/CodeWhiteWeb)
+
+<div align="center">Made by CodeWhiteWeb with ❤ and JavaScript.</div>
--- src/renderer/theme/awesomeCard.js
+++ src/renderer/theme/awesomeCard.js
@@ -1,59 +1,299 @@
 // Define themes.
 const themes = {
   light: {
-    riddle: '333',
-    answer: '2f80ed',
-    background: 'fffefe'
+    riddle: "2f80ed",
+    answer: "434d58",
+    background: "fffefe",
   },
   dark: {
-    riddle: '9f9f9f',
-    answer: 'fff',
-    background: '151515'
-  },
-  'chartreuse-dark': {
-    riddle: 'fff',
-    answer: '7fff00',
-    background: '000'
+    riddle: "fff",
+    answer: "9f9f9f",
+    background: "151515",
   },
   radical: {
-    riddle: 'a9fef7',
-    answer: 'fe428e',
-    background: '141321'
+    riddle: "fe428e",
+    answer: "a9fef7",
+    background: "141321",
   },
   merko: {
-    riddle: '68b587',
-    answer: 'abd200',
-    background: '0a0f0b'
+    riddle: "abd200",
+    answer: "68b587",
+    background: "0a0f0b",
   },
   gruvbox: {
-    riddle: '8ec07c',
-    answer: 'fabd2f',
-    background: '282828'
+    riddle: "fabd2f",
+    answer: "8ec07c",
+    background: "282828",
+  },
+  gruvbox_light: {
+    riddle: "b57614",
+    answer: "427b58",
+    background: "fbf1c7",
   },
   tokyonight: {
-    riddle: '38bdae',
-    answer: '70a5fd',
-    background: '1a1b27'
+    riddle: "70a5fd",
+    answer: "38bdae",
+    background: "1a1b27",
   },
-  catppuccin: {
-    riddle: '96CDFB',
-    answer: 'D9E0EE',
-    background: '161320'
+  onedark: {
+    riddle: "e4bf7a",
+    answer: "df6d74",
+    background: "282c34",
   },
-  algolia: {
-    riddle: '00ADFE',
-    answer: 'FEFEFE',
-    background: '050F2C'
+  cobalt: {
+    riddle: "e683d9",
+    answer: "75eeb2",
+    background: "193549",
   },
-  monokai: {
-    riddle: 'EA1F6A',
-    answer: 'CFCFC9',
-    background: '272822'
+  synthwave: {
+    riddle: "e2e9ec",
+    answer: "e5289e",
+    background: "2b213a",
+  },
+  highcontrast: {
+    riddle: "e7f216",
+    answer: "fff",
+    background: "000",
   },
   dracula: {
-    riddle: 'F8F8F2',
-    answer: '6272A4',
-    background: '282A36'
+    riddle: "ff6e96",
+    answer: "f8f8f2",
+    background: "282a36",
+  },
+  prussian: {
+    riddle: "bddfff",
+    answer: "6e93b5",
+    background: "172f45",
+  },
+  monokai: {
+    riddle: "eb1f6a",
+    answer: "f1f1eb",
+    background: "272822",
+  },
+  vue: {
+    riddle: "41b883",
+    answer: "273849",
+    background: "fffefe",
+  },
+  "vue-dark": {
+    riddle: "41b883",
+    answer: "fffefe",
+    background: "273849",
+  },
+  "shades-of-purple": {
+    riddle: "fad000",
+    answer: "a599e9",
+    background: "2d2b55",
+  },
+  nightowl: {
+    riddle: "c792ea",
+    answer: "7fdbca",
+    background: "011627",
+  },
+  buefy: {
+    riddle: "7957d5",
+    answer: "363636",
+    background: "ffffff",
+  },
+  "blue-green": {
+    riddle: "2f97c1",
+    answer: "0cf574",
+    background: "040f0f",
+  },
+  algolia: {
+    riddle: "00AEFF",
+    answer: "FFFFFF",
+    background: "050F2C",
+  },
+  "great-gatsby": {
+    riddle: "ffa726",
+    answer: "ffd95b",
+    background: "000000",
+  },
+  darcula: {
+    riddle: "BA5F17",
+    answer: "BEBEBE",
+    background: "242424",
+  },
+  bear: {
+    riddle: "e03c8a",
+    answer: "bcb28d",
+    background: "1f2023",
+  },
+  "solarized-dark": {
+    riddle: "268bd2",
+    answer: "859900",
+    background: "002b36",
+  },
+  "solarized-light": {
+    riddle: "268bd2",
+    answer: "859900",
+    background: "fdf6e3",
+  },
+  "chartreuse-dark": {
+    riddle: "7fff00",
+    answer: "fff",
+    background: "000",
+  },
+  nord: {
+    riddle: "81a1c1",
+    answer: "d8dee9",
+    background: "2e3440",
+  },
+  gotham: {
+    riddle: "2aa889",
+    answer: "99d1ce",
+    background: "0c1014",
+  },
+  "material-palenight": {
+    riddle: "c792ea",
+    answer: "a6accd",
+    background: "292d3e",
+  },
+  graywhite: {
+    riddle: "24292e",
+    answer: "24292e",
+    background: "ffffff",
+  },
+  "vision-friendly-dark": {
+    riddle: "ffb000",
+    answer: "ffffff",
+    background: "000000",
+  },
+  "ayu-mirage": {
+    riddle: "f4cd7c",
+    answer: "c7c8c2",
+    background: "1f2430",
+  },
+  "midnight-purple": {
+    riddle: "9745f5",
+    answer: "ffffff",
+    background: "000000",
+  },
+  calm: {
+    riddle: "e07a5f",
+    answer: "ebcfb2",
+    background: "373f51",
+  },
+  "flag-india": {
+    riddle: "ff8f1c",
+    answer: "509E2F",
+    background: "ffffff",
+  },
+  omni: {
+    riddle: "FF79C6",
+    answer: "E1E1E6",
+    background: "191622",
+  },
+  react: {
+    riddle: "61dafb",
+    answer: "ffffff",
+    background: "20232a",
+  },
+  jolly: {
+    riddle: "ff64da",
+    answer: "ffffff",
+    background: "291B3E",
+  },
+  maroongold: {
+    riddle: "F7EF8A",
+    answer: "E0AA3E",
+    background: "260000",
+  },
+  yeblu: {
+    riddle: "ffff00",
+    answer: "ffffff",
+    background: "002046",
+  },
+  blueberry: {
+    riddle: "82aaff",
+    answer: "27e8a7",
+    background: "242938",
+  },
+  slateorange: {
+    riddle: "faa627",
+    answer: "ffffff",
+    background: "36393f",
+  },
+  kacho_ga: {
+    riddle: "bf4a3f",
+    answer: "d9c8a9",
+    background: "402b23",
+  },
+  outrun: {
+    riddle: "ffcc00",
+    answer: "8080ff",
+    background: "141439",
+  },
+  ocean_dark: {
+    riddle: "8957B2",
+    answer: "92D534",
+    background: "151A28",
+  },
+  city_lights: {
+    riddle: "5D8CB3",
+    answer: "718CA1",
+    background: "1D252C",
+  },
+  github_dark: {
+    riddle: "58A6FF",
+    answer: "C3D1D9",
+    background: "0D1117",
+  },
+  discord_old_blurple: {
+    riddle: "7289DA",
+    answer: "FFFFFF",
+    background: "2C2F33",
+  },
+  aura_dark: {
+    riddle: "ff7372",
+    answer: "dbdbdb",
+    background: "252334",
+  },
+  panda: {
+    riddle: "19f9d899",
+    answer: "FF75B5",
+    background: "31353a",
+  },
+  noctis_minimus: {
+    riddle: "d3b692",
+    answer: "c5cdd3",
+    background: "1b2932",
+  },
+  cobalt2: {
+    riddle: "ffc600",
+    answer: "0088ff",
+    background: "193549",
+  },
+  swift: {
+    riddle: "000000",
+    answer: "000000",
+    background: "f7f7f7",
+  },
+  aura: {
+    riddle: "a277ff",
+    answer: "61ffca",
+    background: "15141b",
+  },
+  apprentice: {
+    riddle: "ffffff",
+    answer: "bcbcbc",
+    background: "262626",
+  },
+  moltack: {
+    riddle: "86092C",
+    answer: "574038",
+    background: "F5E1C0",
+  },
+  codeSTACKr: {
+    riddle: "ff652f",
+    answer: "ffffff",
+    background: "09131B",
+  },
+  "rose_pine":{
+    riddle: "9ccfd8",
+    answer: "e0def4",
+    background: "191724",
   }
 };
 
